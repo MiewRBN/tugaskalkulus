73ae53a3cd4568e6e8178caab922ab97db1a5665
MiewRBN <azminursidiq@gmail.com>
2023-11-08 11:03:48 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function calculateDerivative() {
+    const expressionInput = document.getElementById('expression');
+    const resultOutput = document.getElementById('result');
+  
+    const expression = expressionInput.value;
+  
+    try {
+      const derivative = math.derivative(expression, 'x');
+      resultOutput.textContent = derivative.toString();
+    } catch (error) {
+      resultOutput.textContent = 'Terjadi kesalahan dalam perhitungan.';
+    }
+  }
+  
+  
